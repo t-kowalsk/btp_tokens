62b791ac40f65b58c060aedbc66d224d38c5306d
tkowalsk <01178541@pw.edu.pl>
2025-12-08 16:47:15 +0100

    Add first race condition test
    Related to: #3

--- test/wallets_test.go
+++ test/wallets_test.go
@@ -5,11 +5,13 @@ import (
 	database "btp_tokens/internal/pkg/db/migrations/postgres"
 	"btp_tokens/internal/wallets"
 	"bytes"
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 
 	"github.com/99designs/gqlgen/graphql/handler"
@@ -284,4 +286,94 @@ func TestTransferWrongReceiverAddress(t *testing.T) {
         expectedErrorMsg: "transfer fail: recipient wallet not found",
     }
     transferTest(args, initial_wallets)
+}
+
+func TestTransferRaceCondition(t *testing.T) {
+    initial_wallets := []Wallet{
+        {Address: "0x0000000000000000000000000000000000000001", Balance: decimal.NewFromInt(10)},
+        {Address: "0x0000000000000000000000000000000000000002", Balance: decimal.NewFromInt(1)},
+        {Address: "0x0000000000000000000000000000000000000003", Balance: decimal.NewFromInt(0)},
+    }
+
+    db, server := SetUpTest(t, initial_wallets)
+    walletsService := &wallets.WalletsService{DB: db}
+
+    defer database.CloseDB()
+	defer server.Close()
+
+    ready := make(chan struct{}, 3)
+    start := make(chan struct{})
+    results := make(chan string, 3)
+    
+    var wg sync.WaitGroup
+    wg.Add(3)
+
+    amounts := []string{"1", "4", "7"}
+    senders := []string{
+        "0x0000000000000000000000000000000000000002", 
+        "0x0000000000000000000000000000000000000001", 
+        "0x0000000000000000000000000000000000000001"}
+	receivers := []string{
+        "0x0000000000000000000000000000000000000001", 
+        "0x0000000000000000000000000000000000000002", 
+        "0x0000000000000000000000000000000000000003"}
+    
+    for i:= 0; i < 3; i++ {
+        go func (idx int)  {
+            defer wg.Done()
+            ready <- struct{}{}
+
+            <- start
+            
+            mutation := fmt.Sprintf(`
+            mutation {
+                transfer(input: {
+                    from_address: "%s",
+                    to_address: "%s",
+                    amount: "%s"
+                })
+            }
+            `, senders[idx], receivers[idx], amounts[idx])
+
+            resp := doMutation(t, server.URL, mutation)
+            if errors, ok := resp["errors"]; ok {
+                results <- fmt.Sprintf("error:\n from: %v to: %v \n amount: %v \n error msg: %v", senders[idx], receivers[idx], amounts[idx],  errors)
+            } else {
+                results <- fmt.Sprintf("success: \n from: %v to: %v \n amount: %v \n senders updated balance %v", senders[idx], receivers[idx], amounts[idx], resp["data"].(map[string]interface{})["transfer"])
+            }
+        }(i)
+    }
+
+    for i := 0; i < 3; i++ {
+        <-ready
+    }
+
+    close(start)
+
+    wg.Wait()
+    close(results)
+
+    for r := range results {
+        t.Log(r) 
+    }
+
+    balance1, _ := walletsService.GetWalletBalance(context.Background(), "0x0000000000000000000000000000000000000001")
+    balance2, _ := walletsService.GetWalletBalance(context.Background(), "0x0000000000000000000000000000000000000002")
+    balance3, _ := walletsService.GetWalletBalance(context.Background(), "0x0000000000000000000000000000000000000003")
+
+    total := balance1.Add(balance2).Add(balance3)
+    expectedTotal := decimal.NewFromInt(11)
+
+    if !total.Equal(expectedTotal) {
+        t.Fatalf("Final sum mismatch: expected %s, got %s", expectedTotal, total)
+    }
+
+    if balance1.IsNegative() || balance2.IsNegative() || balance3.IsNegative() {
+        t.Fatalf("Negative balance: %s %s %s", balance1, balance2, balance3)
+    }
+
+    t.Log("Final balances:")
+    t.Log("Wallet1:", balance1)
+    t.Log("Wallet2:", balance2)
+    t.Log("Wallet3:", balance3)
 }
