a384f3af0af07a05abdcbc0b54d8719b9089b3dd
tkowalsk <01178541@pw.edu.pl>
2025-12-08 16:46:02 +0100

    Add wallet address tests
    Related to: #3

--- test/wallets_test.go
+++ test/wallets_test.go
@@ -33,23 +33,25 @@ func ResetTestDB() {
     _, _ = database.Db.Exec("TRUNCATE TABLE wallets RESTART IDENTITY CASCADE;")
 }
 
-func SetTwoWallets(firstAddress, secondAddress string, senderBalance, receiverBalance decimal.Decimal) {
-    _, _ = database.Db.Exec(`
+type Wallet struct {
+	Address string
+	Balance decimal.Decimal
+}
+
+func SetWallets(wallets []Wallet) {
+    for _, w := range wallets{
+        _, _ = database.Db.Exec(`
         INSERT INTO wallets (address, balance) VALUES
         ($1, $2) 
         ON CONFLICT (address) DO UPDATE SET balance = $2
-    `, firstAddress, senderBalance)
-    _, _ = database.Db.Exec(`
-        INSERT INTO wallets (address, balance) VALUES
-        ($1, $2)
-        ON CONFLICT (address) DO UPDATE SET balance = $2
-    `, secondAddress, receiverBalance)
+    `, w.Address, w.Balance)
+    }
 }
 
-func SetUpTestTwoWallets(t *testing.T, firstAddress, secondAddress string, senderBalance, receiverBalance decimal.Decimal) (*sql.DB, *httptest.Server) {
+func SetUpTest(t *testing.T, wallets []Wallet) (*sql.DB, *httptest.Server) {
     db := setupTestDB(t)
     ResetTestDB()
-    SetTwoWallets(firstAddress, secondAddress, senderBalance, receiverBalance)
+    SetWallets(wallets)
 
     server := startTestServer(db)
     return db, server
@@ -101,18 +103,16 @@ type transferTestArgs struct {
     t *testing.T 
     fromAddress string
     toAddress string
-    senderBalance string
-    receiverBalance string
     amount string
     expectedKey string
     expectedValue string
     expectedErrorMsg string
 }
 
-func transferTest(args transferTestArgs) {
-    decimalSenderBalance, _ := decimal.NewFromString(args.senderBalance)
-    decimalReceiverBalance, _ := decimal.NewFromString(args.receiverBalance)
-    _, server := SetUpTestTwoWallets(args.t, args.fromAddress, args.toAddress,  decimalSenderBalance, decimalReceiverBalance)
+func transferTest(args transferTestArgs, initial_wallets []Wallet) {
+    // decimalSenderBalance, _ := decimal.NewFromString(args.senderBalance)
+    // decimalReceiverBalance, _ := decimal.NewFromString(args.receiverBalance)
+    _, server := SetUpTest(args.t, initial_wallets)
     defer database.CloseDB()
     defer server.Close()
     mutation := fmt.Sprintf(`
@@ -140,33 +140,148 @@ func transferTest(args transferTestArgs) {
 }
 
 func TestTransferMutationWithDB(t *testing.T) {
+    initial_wallets := []Wallet{
+        {Address: "0x0000000000000000000000000000000000000001", Balance: decimal.NewFromInt(10)},
+        {Address: "0x0000000000000000000000000000000000000002", Balance: decimal.NewFromInt(15)},
+    }
+
     args := transferTestArgs{
         t: t,
         fromAddress: "0x0000000000000000000000000000000000000001",
         toAddress: "0x0000000000000000000000000000000000000002",
-        senderBalance: "10",
-        receiverBalance: "15",
         amount: "6",
         expectedKey: "transfer",
         expectedValue: "4",
         expectedErrorMsg: "",
     }
-    transferTest(args)
-
+    transferTest(args, initial_wallets)
 }
 
-func TestTransferNegative(t *testing.T) {
+func TestTransferMutation(t *testing.T) {
+    initial_wallets := []Wallet{
+        {Address: "0x0000000000000000000000000000000000000001", Balance: decimal.NewFromInt(1000000)},
+        {Address: "0x0000000000000000000000000000000000000002", Balance: decimal.NewFromInt(400000)},
+    }
+
     args := transferTestArgs{
         t: t,
         fromAddress: "0x0000000000000000000000000000000000000001",
         toAddress: "0x0000000000000000000000000000000000000002",
-        senderBalance: "1000",
-        receiverBalance: "500",
+        amount: "700000",
+        expectedKey: "transfer",
+        expectedValue: "300000",
+        expectedErrorMsg: "",
+    }
+    transferTest(args, initial_wallets)
+}
+
+func TestTransferNegative(t *testing.T) {
+    initial_wallets := []Wallet{
+        {Address: "0x0000000000000000000000000000000000000001", Balance: decimal.NewFromInt(1000)},
+        {Address: "0x0000000000000000000000000000000000000002", Balance: decimal.NewFromInt(500)},
+    }
+
+    args := transferTestArgs{
+        t: t,
+        fromAddress: "0x0000000000000000000000000000000000000001",
+        toAddress: "0x0000000000000000000000000000000000000002",
         amount: "-100",
         expectedKey: "errors",
         expectedValue: "",
-        expectedErrorMsg: "Amount must be positive",
+        expectedErrorMsg: "amount must be positive",
     }
-    transferTest(args)
+    transferTest(args, initial_wallets)
 
+}
+
+func TestTransferNotNumeric(t *testing.T) {
+    initial_wallets := []Wallet{
+        {Address: "0x0000000000000000000000000000000000000001", Balance: decimal.NewFromInt(1000)},
+        {Address: "0x0000000000000000000000000000000000000002", Balance: decimal.NewFromInt(500)},
+    }
+
+    args := transferTestArgs{
+        t: t,
+        fromAddress: "0x0000000000000000000000000000000000000001",
+        toAddress: "0x0000000000000000000000000000000000000002",
+        amount: "10q",
+        expectedKey: "errors",
+        expectedValue: "",
+        expectedErrorMsg: "invalid amount format: can't convert 10q to decimal",
+    }
+    transferTest(args, initial_wallets)
+}
+
+func TestTransferFloat(t *testing.T) {
+    initial_wallets := []Wallet{
+        {Address: "0x0000000000000000000000000000000000000001", Balance: decimal.NewFromInt(1000)},
+        {Address: "0x0000000000000000000000000000000000000002", Balance: decimal.NewFromInt(500)},
+    }
+
+
+    args := transferTestArgs{
+        t: t,
+        fromAddress: "0x0000000000000000000000000000000000000001",
+        toAddress: "0x0000000000000000000000000000000000000002",
+        amount: "100.7",
+        expectedKey: "errors",
+        expectedValue: "",
+        expectedErrorMsg: "amount must be an integer (cant be floating point)",
+    }
+    transferTest(args, initial_wallets)
+}
+
+func TestTransferInsufficientBalance(t *testing.T) {
+    initial_wallets := []Wallet{
+        {Address: "0x0000000000000000000000000000000000000001", Balance: decimal.NewFromInt(500)},
+        {Address: "0x0000000000000000000000000000000000000002", Balance: decimal.NewFromInt(300)},
+    }
+
+    args := transferTestArgs{
+        t: t,
+        fromAddress: "0x0000000000000000000000000000000000000001",
+        toAddress: "0x0000000000000000000000000000000000000002",
+        amount: "700",
+        expectedKey: "errors",
+        expectedValue: "",
+        expectedErrorMsg: "insufficient balance",
+    }
+    transferTest(args, initial_wallets)
+}
+
+func TestTransferWrongSenderAddress(t *testing.T) {
+    initial_wallets := []Wallet{
+        {Address: "0x0000000000000000000000000000000000000001", Balance: decimal.NewFromInt(500)},
+        {Address: "0x0000000000000000000000000000000000000002", Balance: decimal.NewFromInt(300)},
+    }
+
+    args := transferTestArgs{
+        t: t,
+        fromAddress: "0x0000000000000000000000000000000000000003",
+        toAddress: "0x0000000000000000000000000000000000000002",
+        amount: "100",
+        expectedKey: "errors",
+        expectedValue: "",
+        expectedErrorMsg: "transfer fail: sender wallet not found",
+    }
+    transferTest(args, initial_wallets)
+}
+
+
+func TestTransferWrongReceiverAddress(t *testing.T) {
+    initial_wallets := []Wallet{
+        {Address: "0x0000000000000000000000000000000000000001", Balance: decimal.NewFromInt(500)},
+        {Address: "0x0000000000000000000000000000000000000002", Balance: decimal.NewFromInt(300)},
+    }
+
+    args := transferTestArgs{
+        t: t,
+        fromAddress: "0x0000000000000000000000000000000000000001",
+        toAddress: "0x0000000000000000000000000000000000000003",
+        amount: "100",
+        expectedKey: "errors",
+        expectedValue: "",
+        expectedErrorMsg: "transfer fail: recipient wallet not found",
+    }
+    transferTest(args, initial_wallets)
 }
